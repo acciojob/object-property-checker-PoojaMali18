0858d2b95dc919e9457b323e1abaaabce6c36f21
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-09-15 22:21:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
-function hasKey(key) {
-  //   write your code here
+function hasKey(key)
+{
+   for (let prop in sampleObject) 
+   {
+    if (prop === key) 
+	{
+      return true;
+    }
+  }
+  return false;
 }
 
 // Do not change the code below
